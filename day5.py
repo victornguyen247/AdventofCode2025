9ad741dff0899d8021c97edcd005ceaa1193e9c1
victornguyen247 <44254369+victornguyen247@users.noreply.github.com>
2025-12-13 19:19:48 -0600

    add check_overlap_2ranges, and union_2ranges

--- day5.py
+++ day5.py
@@ -13,21 +13,39 @@ def prepare_data(path):
     return id_ranges, ids
 
 def validate_ids(id_ranges, ids):
-    valid_ids = {}
-    for (start, end) in id_ranges:
-        for id_ in range(start, end + 1):
-            if valid_ids.get(id_) is None:
-                valid_ids[id_] = True
-    
     num_fresh = 0
-    for id_ in ids:
-        if valid_ids.get(id_) == True:
-            num_fresh += 1
-
+    for id in ids:
+        for start, end in id_ranges:
+            if start <= id  and  id <= end:
+                num_fresh += 1
+                break
     return num_fresh
 
+def total_fresh_ids(id_ranges):
+    union = set()
+    for (start, end) in id_ranges:
+        merged = False
+        for r in id_ranges:
+            if check_overlap_2ranges((start, end), r):
+                (start, end) = union_2sets((start, end), r)
+                merged = True
+
+def check_overlap_2ranges(r1, r2):
+    (s1, e1) = r1
+    (s2, e2) = r2
+    if (s2 <= e1 and s2 >= s1) or (s1 <= e2 and s1 >= s2):
+        return True
+    return False
+    
+def union_2ranges(r1, r2):
+    ''' 2 ranges need to be overlapping or contiguous '''
+    (s1, e1) = r1
+    (s2, e2) = r2
+    urange = (min(s1, s2), max(e1, e2)) 
+    return urange
+
 if __name__ == "__main__":
-    id_ranges, ids = prepare_data("input5-0.txt")
+    id_ranges, ids = prepare_data("input5-1.txt")
     num_fresh = validate_ids(id_ranges, ids)
     print(f"Number of valid IDs: {num_fresh}")
 
