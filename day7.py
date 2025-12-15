1b582b21284eb2d44cebdf454f07fdd5d70f10bb
Victor Nguyen <44254369+victornguyen247@users.noreply.github.com>
2025-12-14 22:25:48 -0600

    finished day 7 part 2

--- day7.py
+++ day7.py
@@ -28,6 +28,38 @@ def count_tachyon(map):
 
     return count
 
+def count_split_beam(map):
+    beam_pos = dict()
+    for j in range(len(map[0])):
+        if map[0][j] == 'S':
+            beam_pos[j] = 1
+    
+    for i in range(0,len(map),2):
+        #if i %2 ==1: print(f"line {i+1}: {beam_pos}")
+        pos = beam_pos.copy()
+        for b in beam_pos:
+            #print(f"b:{b},pos:{pos}")
+            if(map[i][b] =='^'):
+                if(map[i][b-1] != '^'):
+                    if pos.get(b-1) is None:
+                        pos[b-1] = beam_pos[b]
+                    else:
+                        pos[b-1] += beam_pos[b]
+                if(map[i][b+1] != '^'):
+                    if pos.get(b+1) is None:
+                        pos[b+1] = beam_pos[b] 
+                    else:
+                        pos[b+1] += beam_pos[b]
+                pos.pop(b)  
+        beam_pos = pos.copy()
+        print(f"line {i+1}: {beam_pos} \n ---------------------------------")
+    return sum(beam_pos.values())
+
 if __name__ == "__main__":
+    '''# part1
     map = load_data("input7-0.txt")     
-    print(count_tachyon(map))
+    print(count_tachyon(map))'''
+
+    #part2 
+    map = load_data("input7-1.txt")
+    print(count_split_beam(map))
