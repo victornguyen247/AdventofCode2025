33df2554e99a365a984c648606a71d8fd68fd219
victornguyen247 <44254369+victornguyen247@users.noreply.github.com>
2025-12-17 13:11:57 -0600

    finished day8 part1, part2

--- day8.py
+++ day8.py
@@ -18,15 +18,100 @@ def dis_list_asc(data):
     heapq.heapify(l)
     return l
 
-def total_clusters(n:int = 1000, min_heap, data):
-    clusters = list()
+def part1(min_heap, data,n = 1000):
+    nodes = {}
+    clusters = {}
+    cluster_id = 1
     while n > 0:
-        min_dis, node1, node2 = heapq.heappop(min_heap)
-        
+        nodes, clusters, cluster_id,_,_ = connect(min_heap, nodes, clusters, cluster_id)
         n -= 1
     
-if __name__ == "__main__":
-    #part1
-    data = load_data("input8-0.txt")
+    for node in data:
+        node = str(node)
+        if nodes.get(node) is None:
+            nodes[node] = cluster_id
+            clusters[cluster_id] = list([node])
+            cluster_id += 1
+    print(f"nodes: {nodes}")
+    print(f"clusters: {clusters}")  
+    
+    largest_3 = list()
+    for cluster_id in clusters.keys():
+        n = len(clusters[cluster_id])
+        if n not in largest_3:
+            largest_3.append(n)
+    largest_3 = sorted(largest_3, reverse = True)
+    print("largest:", largest_3)
+    product = largest_3[0] * largest_3[1] * largest_3[2]
+    return product             
+ 
+def connect(min_heap, nodes, clusters, cluster_id):
+    min_dis, node1, node2 = heapq.heappop(min_heap)
+    x1 = node1[0]
+    x2 = node2[0]
+    node1 = str(node1)
+    node2 = str(node2)
+    #print(f"node1: {node1}, node2: {node2}, dis:{min_dis}")
+    if nodes.get(node1) == None and nodes.get(node2) == None:
+        #print(f"create new cluster {cluster_id}")
+        nodes[node1] = cluster_id
+        nodes[node2] = cluster_id
+        clusters[cluster_id]= list([node1,node2])
+        #print(f"check cluster:{clusters[cluster_id]}")
+        cluster_id += 1
+    elif nodes.get(node1) == None and nodes.get(node2) != None:
+        #print("add node1 into node 2 cluster ")
+        nodes[node1] = nodes[node2]
+        clusters[nodes[node2]].append(node1)
+        #print("nodes:",nodes, "\n clusters:", clusters)
+    elif nodes.get(node2) == None and nodes.get(node1) != None:
+        #print("add node2 into node1 cluster ")
+        nodes[node2] = nodes[node1]
+        clusters[nodes[node1]].append(node2)
+        #print("nodes:",nodes, "\n clusters:", clusters)
+    else:
+        if nodes[node1] != nodes[node2]:
+            #print("union node2 cluster into node1")
+            cluster_id_node2 = nodes[node2]
+            cluster_id_node1 = nodes[node1]
+            for node in clusters[cluster_id_node2]:
+                nodes[node] = cluster_id_node1
+                clusters[cluster_id_node1].append(node)
+            clusters.pop(cluster_id_node2)  
+            #print("nodes:",nodes, "\n clusters:", clusters)
+        #else:
+            #print("node1 and node2 same cluster")    
+    return nodes, clusters, cluster_id, x1,x2
+    
+def part2(min_heap, n=1000):
+    nodes = {}
+    clusters = {}
+    cluster_id = 1
+    x1 = 0
+    x2 =0
+    while n > 0:
+        nodes, clusters, cluster_id,_,_ = connect(min_heap, nodes, clusters, cluster_id)
+        n -= 1
+    for node in data:
+        node = str(node)
+        if nodes.get(node) is None:
+            nodes[node] = cluster_id
+            clusters[cluster_id] = list([node])
+            cluster_id += 1
+            
+    while(len(clusters)>1):
+        nodes, clusters, cluster_id, x1, x2 = connect(min_heap, nodes, clusters, cluster_id)
+        print(f"x1:{x1}, x2:{x2}")
+    
+    return x1 * x2
+    
+if __name__ == "__main__": 
+    data = load_data("input8-1.txt")
+    #print(f"data: {data}")
     min_heap = dis_list_asc(data)
+    #print(min_heap)
+    #part1
+    #print(part1(min_heap.copy(), data, n= 10))
+    #part2
+    print(part2(min_heap.copy()))
     
