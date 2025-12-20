1ec5f825d734b707b064c23cbe875f40da12fb72
victornguyen247 <44254369+victornguyen247@users.noreply.github.com>
2025-12-20 14:32:07 +0700

    day9 part2

--- day9.py
+++ day9.py
@@ -47,42 +47,43 @@ def part2(data):
 def valid_zone(data):
     ver_zone = {}
     hor_zone = {}
+    i_list = [y for x,y in data]
+    i_list = i_list.sort()
+    j_list = [x for x,y in data] 
+    j_list = j_list.sort()
     for k in range(-1,len(data)-1):
         j1, i1 = data[k]
         j2, i2 = data[k+1]
         #print(f"node {i1},{j1} and {i2},{j2}")
         if (j1 == j2):
-            if ver_zone.get(j1) is None:
-                ver_zone[j1] = list([min(i1,i2), max(i1,i2)])
-            else:
-                ver_zone[j1].append([min(i1,i2), max(i1,i2)])
-        elif(i1 == i2):
-            if hor_zone.get(i1) is None:
-                hor_zone[i1] = list([min(j1,j2), max(j1,j2)])
-            else:
-                hor_zone[i1].append([min(j1,j2), max(j1,j2)])    
-    
-    '''#print("green_red tiles:",gr_tile)
-    blist = {}
-    for node in gr_tile:
-        i,j = node 
-        if blist.get(i) is None:
-            blist[i]= list([j])
-        else:
-            blist[i].append(j)
-    #print("black list:",blist)
-    for id,val in blist.items():
-        if len(val)%2==1:
-            blist[id].pop(-2)'''
+            (i1,i2) = (i2,i1) if i2 < i1 else (i1,i2) 
+            filter_l = [x for x in i_list if i1 <= x <=i2]
+            for m in filter_l:
+                if hor_zone.get(m) is None:
+                    hor_zone[m] = list([j1])
+                else:
+                    hor_zone[m].append([j1])
+        if (i1 == i2):
+            (j1,j2) = (j2,j1) if j2 < j1 else (j1,j2) 
+            filter_l = [y for y in j_list if j1 <= y <=j2]
+            for m in filter_l:
+                if ver_zone.get(m) is None:
+                    ver_zone[m] = list([i1])
+                else:
+                    ver_zone[m].append([i1])            
+            
+    for id,val in hor_zone.items():
+        if len(val)%2 == 1:
+            hor_zone[id].pop(-2)
+    for id,val in ver_zone.items():
+        if len(val)%2 == 1:
+            ver_zone[id].pop(-2)
+    print(f"ver_zone: \n{ver_zone},\n hor_zone: \n{hor_zone}")
     return ver_zone, hor_zone
-    
-def part2_2(data):
-    ver_zone, hor_zone = valid_zone(data)
-    for a in range(1,len(data)-1):
-        for b in range(a):                 
-    
+                    
 if __name__ == "__main__":
     data = load_data("input9-1.txt")
     #print(part1(data))
     #print("data:",data)
-    part2(data)
+    #part2(data)
+    valid_zone(data)
