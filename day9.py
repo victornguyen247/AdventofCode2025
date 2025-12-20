141fac8930024499cde9d3bc85a52d1a98eabd5d
victornguyen247 <44254369+victornguyen247@users.noreply.github.com>
2025-12-20 13:39:01 +0700

    day 9 part2

--- day9.py
+++ day9.py
@@ -16,38 +16,73 @@ def part1(data):
     return max_rec
 
 def part2(data):
-    gb_tile = list() 
-    for k in range(len(data)-1):
+    valid_list = valid_zone(data)
+    max_rec = 0
+    for a in range(1,len(data)-1):
+        for b in range(a):
+            j1, i1 = data[a]
+            j2, i2 = data[b]
+            #print(f"node ({j1},{i1}):({j2},{i2})")
+            valid = True
+            for i in range(min(i1,i2),max(i1,i2)+1):
+                ranges = blist[i]
+                for k in range(0,len(ranges),2):
+                    c = ranges[k]
+                    d = ranges[k+1]
+                    #print(f"+ row{i} range: {c},{d}")
+                    min_j = min(j1,j2)
+                    max_j = max(j1,j2)
+                    if ranges[k] > min_j or max_j > ranges[k+1]:
+                        valid = False 
+                        break
+                if not valid:
+                    break
+            if valid:
+                area = (abs(j2 - j1) +1) * (abs(i2 - i1) +1)
+                #print(f"node: {data[a]},{data[b]}, area = {area}")
+                max_rec = max(max_rec, area)
+    print(max_rec)
+    return max_rec
+
+def valid_zone(data):
+    ver_zone = {}
+    hor_zone = {}
+    for k in range(-1,len(data)-1):
         j1, i1 = data[k]
         j2, i2 = data[k+1]
+        #print(f"node {i1},{j1} and {i2},{j2}")
         if (j1 == j2):
-            (i1,i2) = (i2,i1) if i2 < i1 else (i1,i2) 
-            for m in range(i1,i2):
-                gb_tile.append([m,j1])
-        elif i1 == i2:
-            (j1,j2) = (j2,j1) if j2 < j1 else (j1,j2) 
-            for m in range(j1,j2):
-                gb_tile.append([i1,m])
-    gb_tile = sorted(gb_tile, key=lambda x: x[1])   
-    gb_tile = sorted(gb_tile, key=lambda x: x[0])
-    cp_gb = gb_tile.copy()
-    for i in range(len(gb_tile)-1):
-        if gb_tile[i] == gb_tile[i+1]:
-            cp_gb.pop(i)
-    gb_tile = cp_gb
-    print(gb_tile)
+            if ver_zone.get(j1) is None:
+                ver_zone[j1] = list([min(i1,i2), max(i1,i2)])
+            else:
+                ver_zone[j1].append([min(i1,i2), max(i1,i2)])
+        elif(i1 == i2):
+            if hor_zone.get(i1) is None:
+                hor_zone[i1] = list([min(j1,j2), max(j1,j2)])
+            else:
+                hor_zone[i1].append([min(j1,j2), max(j1,j2)])    
     
-    blist = set()
-    min_i = gb_tile[0][0]
-    max_i = gb_tile[len(gb_tile)-1][0]
-    for node in gb_tile:
-        
-                   
-                    
-        
-        
-
+    '''#print("green_red tiles:",gr_tile)
+    blist = {}
+    for node in gr_tile:
+        i,j = node 
+        if blist.get(i) is None:
+            blist[i]= list([j])
+        else:
+            blist[i].append(j)
+    #print("black list:",blist)
+    for id,val in blist.items():
+        if len(val)%2==1:
+            blist[id].pop(-2)'''
+    return ver_zone, hor_zone
+    
+def part2_2(data):
+    ver_zone, hor_zone = valid_zone(data)
+    for a in range(1,len(data)-1):
+        for b in range(a):                 
+    
 if __name__ == "__main__":
-    data = load_data("input9-0.txt")
+    data = load_data("input9-1.txt")
     #print(part1(data))
+    #print("data:",data)
     part2(data)
